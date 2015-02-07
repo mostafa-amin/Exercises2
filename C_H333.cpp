311e12b79b5dd73de6d2835c4de28699bb9eddba
mostafa <mostafa-amin@live.com>
2015-02-07 07:29:11 +0100

    exercise 3.33

---
+++ C_H333.cpp
@@ -0,0 +1,25 @@
+﻿// This function demonstrates a generic stack.
+#include"stdafx.h"
+using namespace std;
+#include<iostream>
+//#include<stdio.h>
+//#include<stdlib.h>
+
+
+
+int main()
+		{
+			int x,i,j; 
+			cout<<"Please enter the square Side"<<endl;
+			cin>>x;
+			for(i=1;i<=x;i++)
+				{
+					for(j=1;j<=x;j++)
+						printf("*");
+					printf("\n");
+				}
+
+
+
+		
+		}
