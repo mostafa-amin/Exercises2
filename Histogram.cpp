b534bafd935b58feeb3581eec16f4a414b1848d8
mostafa amin <mostafa-amin@live.com>
2015-02-12 18:34:51 +0100

    added solution for 4.18

---
+++ Histogram.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  training
+//
+//  Created by mostafa amin on 07/02/15.
+//  Copyright (c) 2015 mostafa amin. All rights reserved.
+
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+int fact_calc(int x);
+
+
+
+
+int main(int argc, const char * argv[])
+
+    {
+        int i,j;
+        int number;
+        for (i=0; i<5; i++) {
+            cout<<"Please enter the number";
+            cin>>number;
+            
+            for (j=0; j<number; j++) {
+                printf("*");
+            }
+            printf("\n");
+        }
+        
+    
+    }
+
+
